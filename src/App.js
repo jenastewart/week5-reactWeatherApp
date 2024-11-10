400e71dada19e6e319d72c1c401ff9d1a2ecc1bc
Jena Stewart <66694416+jenastewart@users.noreply.github.com>
2024-11-10 13:17:32 -0800

    Added styling

--- src/App.js
+++ src/App.js
@@ -1,19 +1,22 @@
 import "./App.css";
+import Weather from "./Weather";
 
 export default function App() {
   return (
     <div className="App">
-      <h1>Weather App</h1>
-      <footer>
-        This project was coded by Jena Stewaart and is open-sourced on{" "}
-        <a
-          href="https://github.com/jenastewart/week5-reactWeatherApp"
-          target="_blank"
-          rel="noreferrer"
-        >
-          GitHub
-        </a>{" "}
-      </footer>
+      <div className="container">
+        <Weather />
+        <footer>
+          This project was coded by Jena Stewart and is open-sourced on{" "}
+          <a
+            href="https://github.com/jenastewart/week5-reactWeatherApp"
+            target="_blank"
+            rel="noreferrer"
+          >
+            GitHub
+          </a>
+        </footer>
+      </div>
     </div>
   );
 }
