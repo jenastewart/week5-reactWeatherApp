9a7445876149bb9071417e621e1dbdfc5728fbb1
Jena Stewart <66694416+jenastewart@users.noreply.github.com>
2024-11-10 11:43:01 -0800

    Added GitHub repo URL

--- src/App.js
+++ src/App.js
@@ -4,6 +4,16 @@ export default function App() {
   return (
     <div className="App">
       <h1>Weather App</h1>
+      <footer>
+        This project was coded by Jena Stewaart and is open-sourced on{" "}
+        <a
+          href="https://github.com/jenastewart/week5-reactWeatherApp"
+          target="_blank"
+          rel="noreferrer"
+        >
+          GitHub
+        </a>{" "}
+      </footer>
     </div>
   );
 }
