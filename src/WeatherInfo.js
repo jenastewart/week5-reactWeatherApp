1676cafaf2693bc4467f2caabd91f5c6e0ae1ecd
Jena Stewart <66694416+jenastewart@users.noreply.github.com>
2024-11-11 14:44:00 -0800

    Added component to display temp in Celsius or Fahrenheit

--- src/WeatherInfo.js
+++ src/WeatherInfo.js
@@ -1,5 +1,6 @@
 import React from "react";
 import FormattedDate from "./FormattedDate";
+import WeatherTemperature from "./WeatherTemperature";
 
 export default function WeatherInfo(props) {
   return (
@@ -12,12 +13,11 @@ export default function WeatherInfo(props) {
         <li className="text-capitalize">{props.data.description}</li>
       </ul>
       <div className="row mt-3">
-        <div className="col-6">
-          <img src={props.data.iconUrl} alt={props.data.description} />
-          <span className="temperature">
-            {Math.round(props.data.temperature)}
-          </span>
-          <span className="unit">°C</span>
+        <div className="col-6 d-flex flex-row">
+          <div>
+            <img src={props.data.iconUrl} alt={props.data.description} />
+          </div>
+          <WeatherTemperature celsius={props.data.temperature} />
         </div>
         <div className="col-6">
           <ul>
