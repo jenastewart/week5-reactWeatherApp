a76fb59deffe90a01542d042509595b3c26efa96
Jena Stewart <66694416+jenastewart@users.noreply.github.com>
2024-11-17 14:26:53 -0800

    Finalized styling

--- src/WeatherInfo.js
+++ src/WeatherInfo.js
@@ -4,7 +4,7 @@ import WeatherTemperature from "./WeatherTemperature";
 
 export default function WeatherInfo(props) {
   return (
-    <div className="WeatherInfo mb-3">
+    <div className="WeatherInfo mb-3 pl-2">
       <h1>{props.data.city}</h1>
       <ul>
         <li>
