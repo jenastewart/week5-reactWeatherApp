61a1e30492da82130d898b8c2d923b384d899c16
Jena Stewart <66694416+jenastewart@users.noreply.github.com>
2024-11-17 10:19:01 -0800

    Added weather forecast component

---
+++ src/WeatherForecast.js
@@ -0,0 +1,19 @@
+import React from "react";
+import "./WeatherForecast.css";
+
+export default function WeatherForecast() {
+  return (
+    <div className="WeatherForecast">
+      <div className="row">
+        <div className="col">
+          <div className="WeatherForecast-day">Thu</div>
+          <div className="WeatherForecast-icon">⛅</div>
+          <div className="WeatherForecast-temperatures">
+            <span className="WeatherForecast-temperature-max">19</span>
+            <span className="WeatherForecast-temperature-min">1o</span>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
